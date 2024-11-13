b94c79be4582cd6ae1f7d55fbd45a383b68d7852
Carmel1215 <kth12153@gmail.com>
2024-11-13 23:46:55 +0900

    Feat: Log, Warning, Error 출력 함수 구현 및 ConsoleColor 오버로딩

---
+++ TerminalHelper/TermHelper.cs
@@ -0,0 +1,46 @@
+﻿namespace TerminalHelper;
+
+public static class TermHelper
+{
+    public static void WriteLog(string line)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine(line);
+        Console.ResetColor();
+    }
+
+    public static void WriteLog(string line, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(line);
+        Console.ResetColor();
+    }
+
+    public static void WriteWarning(string line)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(line);
+        Console.ResetColor();
+    }
+
+    public static void WriteWarning(string line, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(line);
+        Console.ResetColor();
+    }
+
+    public static void WriteError(string line)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(line);
+        Console.ResetColor();
+    }
+
+    public static void WriteError(string line, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(line);
+        Console.ResetColor();
+    }
+}
